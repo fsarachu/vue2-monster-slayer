7c0d6f9850801936cc8c540d73a38c8df25363e4
fsarachu <fsarachu@gmail.com>
2017-04-01 18:20:47 -0300

    Add computed health bars class names

--- js/app.js
+++ js/app.js
@@ -5,5 +5,37 @@ new Vue({
         minHealth: 0,
         playerHealth: 100,
         monsterHealth: 100
+    },
+    computed: {
+        playerHealthClass: function () {
+            var className;
+
+            if (this.playerHealth <= this.maxHealth / 5) {
+                className = 'is-danger'
+            } else if (this.playerHealth <= this.maxHealth / 2) {
+                className = 'is-warning'
+            } else if (this.playerHealth <= this.maxHealth - 1) {
+                className = 'is-success'
+            } else {
+                className = 'is-primary'
+            }
+
+            return className;
+        },
+        monsterHealthClass: function () {
+            var className;
+
+            if (this.monsterHealth <= this.maxHealth / 5) {
+                className = 'is-danger'
+            } else if (this.monsterHealth <= this.maxHealth / 2) {
+                className = 'is-warning'
+            } else if (this.monsterHealth <= this.maxHealth - 1) {
+                className = 'is-success'
+            } else {
+                className = 'is-primary'
+            }
+
+            return className;
+        }
     }
 });
